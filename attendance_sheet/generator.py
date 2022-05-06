f8d9d23705ec510c68ebdbdbe9ab1a03a3d2dad3
Lucas <lucasbsb100@gmail.com>
2022-05-06 09:12:18 -0300

    Atualização 1.1

--- attendance_sheet/generator.py
+++ attendance_sheet/generator.py
@@ -2,22 +2,55 @@ from pandas import read_csv, read_excel, merge
 from datetime import datetime
 
 def getSpreadSheets(_input):
-    ssInfo = read_csv(_input, sep='\t',encoding='utf-16', on_bad_lines='skip', nrows=6)
-    ssIn = read_csv(_input, sep='\t',encoding='utf-16', on_bad_lines='skip',skiprows=7)
+    global typeCsv
+    ssInfo = read_csv(_input, sep='\t',encoding='utf-16', on_bad_lines='skip', nrows=7)
+    typeCsv = getTypeCsv(ssInfo)
+    if typeCsv == 0:
+        ssIn = read_csv(_input, sep='\t',encoding='utf-16', on_bad_lines='skip',skiprows=7)
+    else:
+        ssIn = read_csv(_input, sep='\t',encoding='utf-16', on_bad_lines='skip',skiprows=8, nrows=ssInfo.iloc[0][0])
     return ssIn, ssInfo
 
 def convertToDatetime(lst):
     date = []
     for line in lst:
-        date.append(datetime.strptime(line, '%m/%d/%Y, %I:%M:%S %p'))
+        if type(line) is str:
+            if typeCsv == 1:
+                line = datetime.strptime(line, '%m/%d/%y, %I:%M:%S %p')
+            else:
+                line = datetime.strptime(line, '%m/%d/%Y, %I:%M:%S %p')
+        date.append(line)
     return date
+
+def getTypeCsv(df):
+    global horaEntrada
+    global horaSaida
+    global duracao
+    if df.columns[0] == '1. Resumo':
+        # Tipo com 2 tabelas
+        horaEntrada = 'Primeiro ingresso'
+        horaSaida = 'Última saída'
+        duracao = 'Duração da\xa0reunião'
+        return 1
+    else:
+        # Tipo com 1 tabela
+        
+        horaEntrada = 'Horário de Entrada'
+        horaSaida = 'Horário de Saída'
+        duracao = 'Duração'
+        return 0
 
 def convertDuration(duration):
     return list(map(lambda x: x.total_seconds(),duration))
 
+    
 def getClassDate(df):
-    dt = convertToDatetime([df.loc['Hora de início da reunião']['Resumo da Reunião']])
-    return dt[0].strftime('%d/%m')
+    dt = df.iloc[2][0]
+    if typeCsv == 1:
+        converted = datetime.strptime(dt, '%m/%d/%y, %I:%M:%S %p')
+    else:
+        converted = datetime.strptime(dt, '%m/%d/%Y, %I:%M:%S %p')
+    return f"{converted.strftime('%d')}/{converted.strftime('%m')}"
 
 def isPresent(spreadsheet):
     presenca = []
@@ -38,37 +71,37 @@ def totalTime(df):
     result.drop_duplicates(subset=['Matrícula'],inplace=True)
     return result
 
-
 def generateAttendenceSheet(ssIn, output, date):
     try:
         ssOut = read_excel(output)
         ssOut = ssOut.astype({'Matrícula': str})
         ssOut = merge(ssOut, ssIn, how='left',on='Matrícula')
         ssOut[date].fillna(int(0), inplace=True)
+        ssOut = ssOut.astype({date: int})
         ssOut.to_excel(output,index=False)
         return True
     except:
         return False
-
+    
 def dataProcessing(ss):
-    # Descartar colunas
-    ss.drop(columns=['Email', 'Função'], inplace=True)
-    # Ordenação por nome
-    ss.sort_values(by='Nome Completo', inplace=True)
+    ss.drop(columns=['Email', 'Função',duracao], inplace=True)        # Ordenação por nome
+    if typeCsv == 1:
+        ss.sort_values(by="Nome", inplace=True)
+    else:
+        ss.sort_values(by="Nome Completo", inplace=True)
     # Obtendo a matricula
     ss.rename(columns={'ID do participante (UPN)': 'Matrícula'}, inplace=True)
     ss['Matrícula'] = getId(ss['Matrícula'])
     # Conversão de datas para o formato datetime
-    ss['Horário de Entrada']= convertToDatetime(ss['Horário de Entrada'])
-    ss['Horário de Saída'] = convertToDatetime(ss['Horário de Saída'])
-    ss['Duração'] = convertDuration(ss['Horário de Saída'] - ss['Horário de Entrada'])
+    ss[horaEntrada]= convertToDatetime(ss[horaEntrada])
+    ss[horaSaida] = convertToDatetime(ss[horaSaida])
+    ss['Duração'] = convertDuration(ss[horaSaida] - ss[horaEntrada])
     ss = totalTime(ss)
     # Remoção de Não alunos
     ss.set_index('Matrícula', inplace=True)
     ss.drop('elainevenson', inplace=True)
-    
     return ss
-    
+
 def main(path_in, path_out):
 
     ss, ssInfo = getSpreadSheets(path_in)
@@ -78,5 +111,5 @@ def main(path_in, path_out):
 
     ss[classDate] = isPresent(ss)
     ss.reset_index(inplace=True)
-
+#     return ss, classDate
     return generateAttendenceSheet(ss[['Matrícula', classDate]], path_out, classDate)
